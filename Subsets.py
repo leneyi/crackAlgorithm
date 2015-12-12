248275101f255ff93348aa8fd148f9bc31f5587f
lingyi wu <wu.lingyi@hotmail.com>
2015-12-12 13:14:37 -0800

    commit of first problem

---
+++ Subsets.py
@@ -0,0 +1,16 @@
+
+
+def subsets(S):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    ans = [[]];
+    if not S:
+      return ans;
+    else:
+      # non-descending order
+      S.sort();
+      ans = subsets(S[1:]);
+      return ans+[[S[0]] + ele for ele in ans];
+subsets([1,2,3]);
