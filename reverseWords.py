24c39d25752c6aa776527cb8c752241b7068e70b
lingyi wu <wu.lingyi@hotmail.com>
2015-12-20 15:48:09 -0800

    Given an input string, reverse the string word by word.

---
+++ reverseWords.py
@@ -0,0 +1,16 @@
+def reverseWords(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    s = s.strip();
+    if len(s) == 0:
+        return '';   
+    s1 = s.split();
+    temp = '';
+    for i in range(len(s1)-1,0,-1):
+        temp +=s1[i]+ ' ';
+    temp +=s1[0];
+    return temp;
+
+print(reverseWords(""));
