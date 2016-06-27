b470112d8be0766f6e7d55cddba9c41dfe81ed2b
lingyi wu <lingyi@lingyis-MBP.attlocal.net>
2016-06-27 11:52:11 -0700

    reverse a linked list from position m to n

---
+++ reverseLinkedList.py
@@ -0,0 +1,44 @@
+class ListNode(object):
+     def __init__(self, x):
+        self.val = x
+        self.next = None
+class Solution(object):
+        def reverseLinkedList(head, m, n):
+            dummyNode = ListNode(0)
+            pre = dummyNode;
+            dummyNode.next = head;
+
+        # if m == n return it.
+            if m == n:
+                return head;
+        #find the pre, which is before cur
+            for i in range(0,m-1):
+                pre = pre.next;
+
+            reverse = None;
+            curr = pre.next;
+
+            for i in range(0, n-m+1):
+                 next = curr.next;
+                 curr.next = reverse;
+                 reverse = curr;
+                 curr = next;
+
+            pre.next.next = curr;
+            pre.next = reverse;
+            print dummyNode.next.val;
+            print dummyNode.next.next.val
+            print dummyNode.next.next.next.val
+            return dummyNode.next;
+        if __name__=="__main__":
+            head = ListNode(5);
+            a = ListNode(6);
+            b = ListNode(7);
+            c = ListNode(8);
+            d = ListNode(9);
+            head.next = a;
+            a.next = b;
+            b.next = c;
+            c.next = d;
+            d.next = None;
+            reverseLinkedList(head, 2, 4);
