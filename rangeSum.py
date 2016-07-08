5e1d1b0631b66adcca9f463acbda664315e897d2
lingyi wu <wu.lingyi@hotmail.com>
2016-07-07 17:47:46 -0700

    Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.

---
+++ rangeSum.py
@@ -0,0 +1,19 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """        
+        self.sumi = nums;
+        for i in range(1, len(nums)):
+            self.sumi[i] += self.sumi[i-1];
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """        
+        if i == 0:
+            return self.sumi[j];
+        return self.sumi[j]-self.sumi[i-1];
